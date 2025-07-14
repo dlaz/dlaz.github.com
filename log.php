2bf6d74d7414b6f089f435ccb8b010f2587e11c8
Dan Lazewatsky <dan@lazewatsky.com>
2025-07-14 12:15:12 -0400

    Remove unused files

--- log.php
+++
@@ -1,8 +0,0 @@
-<pre>
-<?
-$line = sprintf("%s	%s	%s\n", date("F j, Y, g:i a"), $_SERVER['REMOTE_ADDR'], gethostbyaddr($_SERVER['REMOTE_ADDR']));
-$file = fopen('log.txt', 'a');
-fwrite($file, $line);
-fclose($file);
-?>
-</pre>
